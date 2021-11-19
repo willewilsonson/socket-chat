682991d24ec103bb17c03eaf89d9985bc2a3d552
willewilsonson <william.nordenstad@gmail.com>
2021-11-19 12:00:18 +0100

    init

---
+++ server/index.js
@@ -0,0 +1,40 @@
+const express = require('express');
+const http = require('http');
+const cors = require('cors');
+const { Server } = require('socket.io');
+
+const app = express();
+
+app.use(cors());
+
+
+const server = http.createServer(app);
+
+const io = new Server(server, {
+    cors: {
+        origin: 'http://localhost:3000',
+        methods: ['GET', 'POST'],
+    }
+});
+
+io.on('connection', socket => {
+    console.log(`User ${socket.id} connected`);
+
+    socket.on('join_room', data => {
+        socket.join(data);
+        console.log(`User with ID: ${socket.id} joined room: ${data}`);
+    });
+
+    socket.on('send_message', (data) => {
+        socket.to(data.room).emit('recieve_message', data);
+    });
+
+    socket.on('disconnect', () => {
+        console.log(`User ${socket.id} disconnected`);
+    });
+});
+
+
+server.listen(3001, () => {
+    console.log('server running on port 3001');
+});
